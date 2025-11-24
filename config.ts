60131f9ee3d49a5b8e6797ba00df7c750d573011
Rollie111 <mrollheiser18@gmail.com>
2025-11-24 13:17:08 -0600

    Fix syntax error in config.ts

--- config.ts
+++ config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@/types/config";
 
-const config: Config = {
+const config: Config = {npm i
   // Server Configuration
   server: {
     port: 8080, // The port on which Interstellar runs (Default: 8080)
